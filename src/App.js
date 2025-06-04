e44faedf8212da08baea0ddd95ebabb7c48edb2c
Heeba Merchant <114188321+HeebaMerchant@users.noreply.github.com>
2025-06-04 18:57:02 -0400

    Revert "Change"

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import './App.css';
 import Portfolio from './components/Portfolio.jsx';
-import SympliPage from './components/SympliPage.jsx';
-import EffectiveTeamDynamics from './components/EffectiveTeamDynamics.jsx'
-import FlightDelayedPrediction from './components/FlightDelayedPrediction.jsx'
-import AtlantaFoodFinder from './components/AtlantaFoodFinder.jsx';
-import EmailPhishingDetection from './components/EmailPhishingDetection.jsx'
-import MediaProjectDemo from './components/ObjectModeling.jsx'
-import ScrollToTop from './components/ScrollToTop';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<div className="App"><Portfolio /></div>} />
-        <Route path="/sympli" element={<div className="App"><SympliPage /></div>} />
-        <Route path="/fdp" element={<div className="App"><FlightDelayedPrediction /></div>} />
-        <Route path="/etd" element={<div className="App"><EffectiveTeamDynamics /></div>} />
-        <Route path="/atlFoodFinder" element={<div className="App"><AtlantaFoodFinder /></div>} />
-        <Route path="/EmailPhishingDetection" element={<div className="App"><EmailPhishingDetection /></div>} />
-        <Route path="/MediaProjectDemo" element={<div className="App"><MediaProjectDemo /></div>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Portfolio />
+    </div>
   );
 }
 
